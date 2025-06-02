42fd9a2f6ef206b3340eb215b26473f594e74702
MoisesQuiruchiToledo <143120698+MoisesQuiruchiToledo@users.noreply.github.com>
2025-06-02 14:00:41 -0400

    Create Famili.pl

---
+++ Famili.pl
@@ -0,0 +1,61 @@
+% Definición de miembros de la familia
+padre(jose).
+madre(erminia).
+
+% Hijos e hijas de Jose y Erminia
+hijo(isaias, jose).
+hijo(moises, jose).
+hija(ruth, jose).
+hija(lin, jose).
+hijo(isaias, erminia).
+hijo(moises, erminia).
+hija(ruth, ermminia).
+hija(lin, erminia).
+% Hijas de Isaias y Diana
+hija(jhisel, isaias).
+hija(aylin, isaias).
+hija(jhisel, diana).
+hija(aylin, diana).
+% nuera de Jose y Erminia
+nuera(diana).
+% Cuñada (hermana del esposo o esposa)
+cunada(diana, ruth).
+cunada(diana, lin).
+cunada(diana, moises).
+% Sobrinas (hijas de hermanos o hermanas)
+sobrina(jhisel, moises).
+sobrina(aylin, moises).
+sobrina(jhisel, ruth).
+sobrina(aylin, ruth).
+sobrina(jhisel, lin).
+sobrina(aylin, lin).
+% Hermanos hijos de Jose y Erminia
+hermano(isaias, moises).
+hermano(isaias, ruth).
+hermano(isaias, lin).
+hermano(moises, isaias).
+hermano(moises, ruth).
+hermano(moises, lin).
+hermana(ruth, isaias).
+hermana(ruth, moises).
+hermana(ruth, lin).
+hermana(lin, isaias).
+hermana(lin, moises).
+hermana(lin, ruth).
+% Tios de sobrinas
+sobrina(jhisel, moises).
+sobrina(aylin, moises).
+sobrina(jhisel, ruth).
+sobrina(aylin, ruth).
+sobrina(jhisel, lin).
+sobrina(aylin, lin).
+% nietos de Jose y Erminia
+nieta(jhisel, jose).
+nieta(aylin, jose).
+nieta(jhisel, erminia).
+nieta(aylin, erminia).
+% abuelos de los hijos de Jose y Erminia
+abuelo(jose, jhisel).
+abuelo(jose, aylin).
+abuelo(erminia, jhisel).
+abuelo(erminia, aylin).
